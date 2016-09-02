18c1376e299cb1f50b129097665a93338e22eebf
eljefeo <jeffnavabiz@yahoo.com>
2016-09-01 18:28:53 -0700

    initial prevent close code -

--- js/background.js
+++ js/background.js
@@ -1,6 +1,30 @@
-var enabled = false,
+var enabled = true,
+    preventCloseEnabled = false,
     tabToCopy = 0,
-    myTabs = [];
+    myTabs = [],
+    myWindows = [];
+
+chrome.windows.onRemoved.addListener(function(windowid) {
+ alert("window closed");
+    var ind = myWindows.indexOf(windowid);
+    myWindows.splice(ind, 1);
+    console.log('window closed : id= ' + windowid);
+ //confirm("Press a button!");
+});
+
+chrome.windows.onCreated.addListener(function(windowid) {
+ //alert("window closed");
+ myWindows.push(windowid.id);
+ console.log('window count ' + myWindows.length);
+
+    console.log('window added : id= ' + windowid.id);
+    for(var i=0;i<myWindows.length;i++){
+        console.log("window entry " + myWindows[i]);
+    }
+// confirm("Press a basdasdutton!");
+});
+
+
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
@@ -8,29 +32,45 @@ chrome.runtime.onMessage.addListener(
             enabled = !enabled;
             sendDetails(enabled);
             sendResponse({
-                result: enabled
+                result: enabled,
+                otherResult: preventCloseEnabled
             });
         } else if (request.greeting === "getEnabled") {
             sendResponse({
                 result: enabled
             });
-        } else if (request.greeting === "imActiveTab") {
-            console.log("new tab to select " + sender.tab.id);
-          tabToCopy = sender.tab.id;
+        } else if (request.greeting === "getBothEnabled") {
+            sendResponse({
+                result: enabled,
+                otherResult: preventCloseEnabled
+            });
+        }else if (request.greeting === "imActiveTab") {
+            tabToCopy = sender.tab.id;
         } else if (request.greeting === "doSingleSelect") {
-            console.log("telling tab " + tabToCopy +  " to do single select");
-
             chrome.tabs.sendMessage(tabToCopy, {
                 greeting: 'doSelectSingle'
             }, function(response) {});
         } else if (request.greeting === "doRangeSelect") {
-            console.log("telling tab " + tabToCopy +  " to do range select");
-
             chrome.tabs.sendMessage(tabToCopy, {
                 greeting: 'doSelectRange'
             }, function(response) {});
+        } else if (request.greeting === "changeSelected") {
+            console.log('Tab ' + sender.tab.id + ' change selected');
+            chrome.tabs.update(sender.tab.id, {selected: true});
+        } else if (request.greeting === "changePreventCloseEnabled") {
+            preventCloseEnabled = !preventCloseEnabled;
+            sendPreventCloseDetails(preventCloseEnabled);
+            sendResponse({
+                result: enabled,
+                otherResult: preventCloseEnabled
+            });
+        } else if (request.greeting === "getPreventCloseEnabled") {
+            sendPreventCloseDetails({
+                otherResult: preventCloseEnabled
+            });
         }
-return true;
+
+        return true;
     });
 
 function sendDetails(sendData) {
@@ -39,6 +79,17 @@ function sendDetails(sendData) {
             chrome.tabs.sendMessage(tabs[i].id, {
                 greeting: 'updateEnabled',
                 result: enabled
+            }, function(response) {});
+        }
+    });
+}
+
+function sendPreventCloseDetails(sendData) {
+    chrome.tabs.query({}, function(tabs) {
+        for (var i = 0; i < tabs.length; ++i) {
+            chrome.tabs.sendMessage(tabs[i].id, {
+                greeting: 'updatePreventCloseEnabled',
+                result: preventCloseEnabled
             }, function(response) {});
         }
     });
